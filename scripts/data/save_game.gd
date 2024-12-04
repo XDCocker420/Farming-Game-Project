a89d99c54184b1fa686b5b4561ee0d30774badac
Oliver <20210207@htlwrn.ac.at>
2024-12-04 13:06:24 +0100

    bug fixing für clemens und bill

--- scripts/data/save_game.gd
+++ scripts/data/save_game.gd
@@ -17,150 +17,150 @@ var exp_level:int = 0
 @onready var player = get_tree().get_first_node_in_group("Player")
 
 func _ready() -> void:
-	# Start the auto-save timer
-	start_auto_save_timer()
-	# Attempt to load existing game data
-	load_game()
+    # Start the auto-save timer
+    start_auto_save_timer()
+    # Attempt to load existing game data
+    load_game()
 
-	var err = config.load("res://scripts/config/level_config.cfg")
-	if err != OK:
-		return
+    var err = config.load("res://scripts/config/level_config.cfg")
+    if err != OK:
+        return
 
 
 func save_game() -> void:
-	var save := SavedData.new()
-	save.player_position = player.global_position
-	save.player_level = level
-	save.player_experience_per_level = exp_level
+    var save := SavedData.new()
+    save.player_position = player.global_position
+    save.player_level = level
+    save.player_experience_per_level = exp_level
 
-	var saved_data:Array[ItemSaves] = []
-	get_tree().call_group("game_events", "on_save_game", saved_data)
-	
-	save.saved_data = saved_data
-	save.inventory = inventory
-	
-	ResourceSaver.save(save, SAVE_FILE_PATH)
+    var saved_data:Array[ItemSaves] = []
+    get_tree().call_group("game_events", "on_save_game", saved_data)
+    
+    save.saved_data = saved_data
+    save.inventory = inventory
+    
+    ResourceSaver.save(save, SAVE_FILE_PATH)
 
 
 func load_game() -> void:
-	var saved_game:SavedData = ResourceLoader.load(SAVE_FILE_PATH)
-	if saved_game == null:
-		level = 1
-		inventory.money = 100
-		return
-	
-	get_tree().call_group("game_events", "on_before_load_game")
-	inventory = saved_game.inventory
-	level = saved_game.player_level
-	exp_level = saved_game.player_experience_per_level
-	player.global_position = saved_game.player_position
-	
-	for item in saved_game.saved_data:
-		var scene := load(item.scene_path) as PackedScene
-		var restored_node = scene.instantiate()
+    var saved_game:SavedData = ResourceLoader.load(SAVE_FILE_PATH)
+    if saved_game == null:
+        level = 1
+        inventory.money = 100
+        return
+    
+    get_tree().call_group("game_events", "on_before_load_game")
+    inventory = saved_game.inventory
+    level = saved_game.player_level
+    exp_level = saved_game.player_experience_per_level
+    player.global_position = saved_game.player_position
+    
+    for item in saved_game.saved_data:
+        var scene := load(item.scene_path) as PackedScene
+        var restored_node = scene.instantiate()
 
-		if item is CropSaves:
-			#print(item.parent_path)
-			var temp:Node = get_node(item.parent_path)
-			temp.add_child(restored_node)
-		else:
-			map.add_child(restored_node)
-		if restored_node.has_method("on_load_game"):
-			restored_node.on_load_game(item)
+        if item is CropSaves:
+            #print(item.parent_path)
+            var temp:Node = get_node(item.parent_path)
+            temp.add_child(restored_node)
+        else:
+            map.add_child(restored_node)
+        if restored_node.has_method("on_load_game"):
+            restored_node.on_load_game(item)
 
 
 func add_to_inventory(item:String, count:int=1) -> void:
-	if count < 1:
-		push_error("count musst be bigger or equal 1")
-		get_tree().quit()	
-	if inventory.data.has(item):
-		inventory.data[item] += count
-	else:
-		inventory.data[item] = count
+    if count < 1:
+        push_error("count musst be bigger or equal 1")
+        get_tree().quit()	
+    if inventory.data.has(item):
+        inventory.data[item] += count
+    else:
+        inventory.data[item] = count
 
 # removes something
 func remove_from_inventory(item: String, count:int=1, remove_completly:bool=false) -> void:
-	if count < 1:
-		push_error("count musst be bigger or equal 1")
-		get_tree().quit()
-	if inventory == null or not inventory.data.has(item):
-		push_error("Item not found in Inventory. Add it first")
-		get_tree().quit()
-	if remove_completly:
-		inventory.data.erase(item)
-		return
+    if count < 1:
+        push_error("count musst be bigger or equal 1")
+        get_tree().quit()
+    if inventory == null or not inventory.data.has(item):
+        push_error("Item not found in Inventory. Add it first")
+        get_tree().quit()
+    if remove_completly:
+        inventory.data.erase(item)
+        return
 
-	inventory.data[item] -= count
-	if inventory.data[item] <= 0:
-		inventory.data.erase(item)
-		
+    inventory.data[item] -= count
+    if inventory.data[item] <= 0:
+        inventory.data.erase(item)
+        
 func get_inventory() -> Dictionary:
-	return inventory.data
-	
+    return inventory.data
+    
 func get_item_count(item:String) -> int:
-	if inventory == null or not inventory.data.has(item):
-		return 0
-	return inventory.data[item]
+    if inventory == null or not inventory.data.has(item):
+        return 0
+    return inventory.data[item]
 
 func get_current_level() -> int:
-	return level
+    return level
 
 func get_experience_per_level() -> int:
-	return exp_level
+    return exp_level
 
 func add_experience_points(count:int) -> void:
-	if count < 1:
-		push_error("count musst be bigger or equal 1")
-		get_tree().quit()
-	exp_level += count
-	_check_new_level()
+    if count < 1:
+        push_error("count musst be bigger or equal 1")
+        get_tree().quit()
+    exp_level += count
+    _check_new_level()
 
 func _check_new_level() -> void:
-	if(level > MAX_LEVEL):
-		print("Congratulations you reached the maximum Level")	
-		return
-	var xp_needed = config.get_value("Level" + str(level), "exp_needed")
-	if(exp_level >= xp_needed):
-		level += 1
-		exp_level -= xp_needed
-		print("Congratulations you reached Level " + str(level))
-		if(exp_level < 0):
-			exp_level = 0
+    if(level > MAX_LEVEL):
+        print("Congratulations you reached the maximum Level")	
+        return
+    var xp_needed = config.get_value("Level" + str(level), "exp_needed")
+    if(exp_level >= xp_needed):
+        level += 1
+        exp_level -= xp_needed
+        print("Congratulations you reached Level " + str(level))
+        if(exp_level < 0):
+            exp_level = 0
 
 func add_money(count:int=1) -> void:
-	if count < 1:
-		push_error("count musst be bigger or equal 1")
-		get_tree().quit()
-	inventory.money += count
+    if count < 1:
+        push_error("count musst be bigger or equal 1")
+        get_tree().quit()
+    inventory.money += count
 
 func remove_money(count:int=1) -> bool:
-	if count < 1:
-		push_error("count musst be bigger or equal 1")
-		get_tree().quit()
-	if inventory.money - count < 0:
-		push_warning("Money isn't enough for this interaction")
-		return false
-	inventory.money -= count
-	return true
+    if count < 1:
+        push_error("count musst be bigger or equal 1")
+        get_tree().quit()
+    if inventory.money - count < 0:
+        push_warning("Money isn't enough for this interaction")
+        return false
+    inventory.money -= count
+    return true
 
 func get_money() -> int:
-	return inventory.money
+    return inventory.money
 
 func start_auto_save_timer() -> void:
-	var timer = Timer.new()
-	timer.wait_time = AUTO_SAVE_INTERVAL
-	timer.autostart = true
-	timer.timeout.connect(_on_auto_save_timeout)
-	add_child(timer)
+    var timer = Timer.new()
+    timer.wait_time = AUTO_SAVE_INTERVAL
+    timer.autostart = true
+    timer.timeout.connect(_on_auto_save_timeout)
+    add_child(timer)
 
 
 func _on_auto_save_timeout() -> void:
-	save_game()
+    save_game()
 
 
 func _notification(what):
-	if what == NOTIFICATION_WM_CLOSE_REQUEST:
-		# Defer the save to ensure other nodes have saved their state
-		call_deferred("save_game")
-		# Allow the application to quit
-		get_tree().quit()
+    if what == NOTIFICATION_WM_CLOSE_REQUEST:
+        # Defer the save to ensure other nodes have saved their state
+        call_deferred("save_game")
+        # Allow the application to quit
+        get_tree().quit()
--- scripts/farming/field.gd
+++ scripts/farming/field.gd
@@ -8,84 +8,84 @@ var farming_ui: Node = null
 @onready var selection_highlight = $SelectionHighlight
 
 func _ready() -> void:
-	mouse_entered.connect(_on_mouse_entered)
-	mouse_exited.connect(_on_mouse_exited)
-	input_event.connect(_on_input_event)
-	
-	if not has_node("SelectionHighlight"):
-		var highlight = ColorRect.new()
-		highlight.name = "SelectionHighlight"
-		
-		highlight.color = Color(1, 1, 0, 0.3)
-		highlight.mouse_filter = Control.MOUSE_FILTER_IGNORE
-		highlight.visible = false
-		highlight.size = Vector2(64, 64)
-		highlight.position = Vector2(-32, -32)
-		add_child(highlight)
-		selection_highlight = highlight
-	
-	# Wait for UI to be ready
-	call_deferred("_connect_ui")
+    mouse_entered.connect(_on_mouse_entered)
+    mouse_exited.connect(_on_mouse_exited)
+    input_event.connect(_on_input_event)
+    
+    if not has_node("SelectionHighlight"):
+        var highlight = ColorRect.new()
+        highlight.name = "SelectionHighlight"
+        
+        highlight.color = Color(1, 1, 0, 0.3)
+        highlight.mouse_filter = Control.MOUSE_FILTER_IGNORE
+        highlight.visible = false
+        highlight.size = Vector2(64, 64)
+        highlight.position = Vector2(-32, -32)
+        add_child(highlight)
+        selection_highlight = highlight
+    
+    # Wait for UI to be ready
+    call_deferred("_connect_ui")
 
 func _connect_ui() -> void:
-	var uis = get_tree().get_nodes_in_group("farming_ui")
-	if uis.size() > 0:
-		farming_ui = uis[0]
-		farming_ui.plant_requested.connect(_on_plant_requested)
-		farming_ui.water_requested.connect(_on_water_requested)
-		farming_ui.harvest_requested.connect(_on_harvest_requested)
-		_update_ui_visibility()
+    var uis = get_tree().get_nodes_in_group("farming_ui")
+    if uis.size() > 0:
+        farming_ui = uis[0]
+        farming_ui.plant_requested.connect(_on_plant_requested)
+        farming_ui.water_requested.connect(_on_water_requested)
+        farming_ui.harvest_requested.connect(_on_harvest_requested)
+        _update_ui_visibility()
 
 func _any_field_selected() -> bool:
-	for field in get_tree().get_nodes_in_group("fields"):
-		if field.is_selected:
-			return true
-	return false
+    for field in get_tree().get_nodes_in_group("fields"):
+        if field.is_selected:
+            return true
+    return false
 
 func _update_ui_visibility() -> void:
-	if farming_ui:
-		if _any_field_selected():
-			farming_ui.show_ui()
-		else:
-			farming_ui.hide_ui()
+    if farming_ui:
+        if _any_field_selected():
+            farming_ui.show_ui()
+        else:
+            farming_ui.hide_ui()
 
 func _on_mouse_entered() -> void:
-	selection_highlight.color = Color(1, 1, 0, 0.5)
-	selection_highlight.visible = true
+    selection_highlight.color = Color(1, 1, 0, 0.5)
+    selection_highlight.visible = true
 
 func _on_mouse_exited() -> void:
-	if not is_selected:
-		selection_highlight.visible = false
-	else:
-		selection_highlight.color = Color(1, 1, 0, 0.3)
+    if not is_selected:
+        selection_highlight.visible = false
+    else:
+        selection_highlight.color = Color(1, 1, 0, 0.3)
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		is_selected = !is_selected
-		selection_highlight.visible = is_selected
-		_update_ui_visibility()
+    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+        is_selected = !is_selected
+        selection_highlight.visible = is_selected
+        _update_ui_visibility()
 
 func _on_plant_requested() -> void:
-	if is_selected and not has_node("Carrot"):
-		crop = carrot_scene.instantiate()
-		add_child(crop)
-		print("Plant planted!")
+    if is_selected and not has_node("Carrot"):
+        crop = carrot_scene.instantiate()
+        add_child(crop)
+        print("Plant planted!")
 
 func _on_water_requested() -> void:
-	if is_selected and has_node("Carrot"):
-		var plant = get_node("Carrot")
-		if plant.has_method("water"):
-			if plant.water():
-				selection_highlight.color = Color(0, 0, 1, 0.3)
-				print("Field watered!")
+    if is_selected and has_node("Carrot"):
+        var plant = get_node("Carrot")
+        if plant.has_method("water"):
+            if plant.water():
+                selection_highlight.color = Color(0, 0, 1, 0.3)
+                print("Field watered!")
 
 func _on_harvest_requested() -> void:
-	if is_selected and has_node("Carrot"):
-		var plant = get_node("Carrot")
-		if plant.has_method("harvest") and plant.has_method("can_harvest"):
-			if plant.can_harvest():
-				plant.harvest()
-				is_selected = false
-				selection_highlight.visible = false
-				selection_highlight.color = Color(1, 1, 0, 0.3)
-				_update_ui_visibility()
+    if is_selected and has_node("Carrot"):
+        var plant = get_node("Carrot")
+        if plant.has_method("harvest") and plant.has_method("can_harvest"):
+            if plant.can_harvest():
+                plant.harvest()
+                is_selected = false
+                selection_highlight.visible = false
+                selection_highlight.color = Color(1, 1, 0, 0.3)
+                _update_ui_visibility()
