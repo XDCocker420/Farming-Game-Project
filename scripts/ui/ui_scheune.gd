3a5225dc96fd09d3b5eb5363decd833c1494207f
Daniel.Fass <Fass.Daniel@Student.htlwrn.ac.at>
2024-12-05 09:56:12 +0100

    final scheune ui

--- scripts/ui/ui_scheune.gd
+++ scripts/ui/ui_scheune.gd
@@ -2,9 +2,9 @@ extends Control
 
 
 func _ready() -> void:
-	SaveGame.add_to_inventory("carrot", 5)
-	SaveGame.add_to_inventory("wheat", 2)
-	SaveGame.add_to_inventory("corn", 3)
+	#SaveGame.add_to_inventory("carrot", 5)
+	#SaveGame.add_to_inventory("wheat", 2)
+	#SaveGame.add_to_inventory("corn", 30)
 	load_inventory()
 	visible = false
 
@@ -20,20 +20,27 @@ func load_inventory() -> void:
 	var vbox = $Menu/ScrollContainer/VBoxContainer
 	var slots = $Menu/ScrollContainer/VBoxContainer/GridContainer
 	var slot = preload("res://scenes/ui/ui_slot.tscn")
+	
 	var inventory = SaveGame.get_inventory()
 	var count = 0
 
-	for item in inventory.keys():
+	var keys = inventory.keys()
+	keys.sort_custom(func(x:String, y:String) -> bool: return inventory[x] > inventory[y])
+
+	for item in keys:
 		var new_slot = slot.instantiate()
-		var temp = Sprite2D.new()
-		temp.texture = load("res://assets/gui/icons/" + item + ".png")
-		new_slot.add_child(temp)
+
+		if new_slot.has_node("Icon"):
+			new_slot.get_node("Icon").texture = load("res://assets/gui/icons/" + item + ".png")
+			
 		slots.add_child(new_slot)
+		
 		new_slot.show()
 		count = SaveGame.get_item_count(str(item))
 
-		if count > 1:
+		if count >= 1:
 			new_slot.get_node("Node2D/amount").text = str(count)
+			new_slot.get_node("Node2D/amount").show()
 		else:
 			new_slot.get_node("Node2D/amount").hide()
 	
