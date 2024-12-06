7622a01c8565c9bddf260473de79de3b775fbed8
Bill Meixner <20210229@htlwrn.ac.at>
2024-12-06 12:27:24 +0100

    refactored everything to ui_farming

--- scripts/farming/farming_area.gd
+++ scripts/farming/farming_area.gd
@@ -4,7 +4,7 @@ var in_area: bool = false
 var current_mode: String = ""
 var is_current_area: bool = false
 
-@onready var farming_ui = $FarmingUI
+@onready var ui_farming = $FarmingUI
 @onready var plant_mode_ui = $PlantModeUI
 @onready var water_mode_ui = $WaterModeUI
 @onready var harvest_mode_ui = $HarvestModeUI
@@ -12,7 +12,7 @@ var is_current_area: bool = false
 func _ready() -> void:
 	body_entered.connect(_on_player_entered)
 	body_exited.connect(_on_player_exited)
-	farming_ui.visible = false
+	ui_farming.visible = false
 	plant_mode_ui.visible = false
 	water_mode_ui.visible = false
 	harvest_mode_ui.visible = false
@@ -21,10 +21,10 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel") and current_mode != "": # ESC-Taste
 		exit_mode()
 	elif event.is_action_pressed("ui_accept") and is_current_area and current_mode == "": # E-Taste nur wenn kein Modus aktiv
-		if farming_ui.visible:
-			farming_ui.hide_ui()
+		if ui_farming.visible:
+			ui_farming.hide_ui()
 		else:
-			farming_ui.show_ui()
+			ui_farming.show_ui()
 
 func _on_player_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
@@ -37,7 +37,7 @@ func _on_player_exited(body: Node2D) -> void:
 		is_current_area = false
 		if current_mode != "":
 			exit_mode()
-		farming_ui.hide_ui()
+		ui_farming.hide_ui()
 
 func enter_mode(mode: String) -> void:
 	current_mode = mode
@@ -78,4 +78,4 @@ func exit_mode() -> void:
 	
 	# Zeige das UI wieder an, anstatt es komplett zu schließen
 	if is_current_area:
-		farming_ui.show_ui()
+		ui_farming.show_ui()
