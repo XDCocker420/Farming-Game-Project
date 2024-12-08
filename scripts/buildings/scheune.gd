b5d52d291dbaa344319fae4b262e87151a841599
Fass.Daniel <Fass.Daniel@Student.htlwrn.ac.at>
2024-12-08 14:19:57 +0100

    scheune bug fixes

--- scripts/buildings/scheune.gd
+++ scripts/buildings/scheune.gd
@@ -5,7 +5,6 @@ extends Node2D
 @onready var e_button: AnimatedSprite2D = $AnimatedSprite2D2
 @onready var door: AnimatedSprite2D = $AnimatedSprite2D
 @onready var door_area: Area2D = $DoorArea
-@onready var storage_label: Label = $StorageLabel
 @onready var ui = $UI
 
 var in_area = false
@@ -17,17 +16,14 @@ func _ready() -> void:
 	#if not 'inventory' in SaveData.data:
 		#SaveData.data['player']['inventory']
 	e_button.visible = false
-	storage_label.visible = false
 
 	player.interact.connect(_on_player_interact)
-	player.interact2.connect(_on_player_interact2)
 	door_area.body_entered.connect(_on_door_area_body_entered)
 	door_area.body_exited.connect(_on_door_area_body_exited)
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") && in_area:
-		print("test passed")
 		ui.show()
 
 
@@ -37,30 +33,17 @@ func _on_player_interact():
 			door.play("open")
 			open = true
 			ui.show()
-			storage_label.visible = true
-			update_storage_text()
 		else:
 			door.play_backwards("open")
 			open = false
 			ui.hide()
-			storage_label.visible = false
-
-
-func _on_player_interact2():
-	if in_area and open:
-		if SaveGame.get_inventory():
-			#SaveData.data['inventory'] -= 1
-			update_storage_text()
-
-
-func update_storage_text() -> void:
-	storage_label.text = "Lager: " + str(0) + "/" + str(max_storage)
 
 
 func _on_door_area_body_entered(body):
 	if body.is_in_group("Player"):
 		e_button.visible = true
 		in_area = true
+		
 
 
 func _on_door_area_body_exited(body):
@@ -71,4 +54,4 @@ func _on_door_area_body_exited(body):
 		if open:
 			door.play_backwards("open")
 			open = false
-			storage_label.visible = false
+			ui.hide()
--- scripts/ui/ui_scheune.gd
+++ scripts/ui/ui_scheune.gd
@@ -2,9 +2,9 @@ extends Control
 
 
 func _ready() -> void:
-	#SaveGame.add_to_inventory("carrot", 5)
-	#SaveGame.add_to_inventory("wheat", 2)
-	#SaveGame.add_to_inventory("corn", 30)
+	SaveGame.add_to_inventory("carrot", 5)
+	SaveGame.add_to_inventory("wheat", 2)
+	SaveGame.add_to_inventory("corn", 30)
 	load_inventory()
 	visible = false
 
