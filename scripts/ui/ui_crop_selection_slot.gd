76c63a834deacbf097a63f3b884a4ea7adb02599
Hydro2114 <Meixner.Bill@Student.htlwrn.ac.at>
2024-12-10 02:10:09 +0100

    enabled custon names for slots

--- scripts/ui/ui_crop_selection_slot.gd
+++ scripts/ui/ui_crop_selection_slot.gd
@@ -1,14 +1,21 @@
 extends PanelContainer
 
+var display_names = {
+	"cauliflower": "Cauli",
+}
+
 func _ready() -> void:
 	pass
 
 func _on_icon_mouse_entered() -> void:
 	var name_label = $Node2D/name
 	var icon_name = $Icon.texture.get_path().get_file().get_basename()
-
-	icon_name = icon_name[0].to_upper() + icon_name.substr(1,-1)
-	name_label.text = icon_name
+	
+	if icon_name in display_names:
+		name_label.text = display_names[icon_name]
+	else:
+		icon_name = icon_name[0].to_upper() + icon_name.substr(1,-1)
+		name_label.text = icon_name
 	name_label.show()
 
 func _on_icon_mouse_exited() -> void:
