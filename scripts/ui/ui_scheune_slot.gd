918e4bdd6fdc58a4fab84c2d7f8bc4efc1638a4d
Bill Meixner <20210229@htlwrn.ac.at>
2024-12-12 09:05:47 +0100

    outsourced display_names dictionary to crop manager

--- scripts/ui/ui_scheune_slot.gd
+++ scripts/ui/ui_scheune_slot.gd
@@ -1,10 +1,5 @@
 extends PanelContainer
 
-var display_names = {
-	"cauliflower": "Cauli",
-	"aubergine": "Eggplant"
-}
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,14 +13,8 @@ func _ready() -> void:
 func _on_icon_mouse_entered() -> void:
 	var name_label = $Node2D/name
 	var icon_name = $Icon.texture.get_path().get_file().get_basename()
-
-	if icon_name in display_names:
-		name_label.text = display_names[icon_name]
-	else:
-		icon_name = icon_name[0].to_upper() + icon_name.substr(1,-1)
-		name_label.text = icon_name
+	name_label.text = CropManager.get_display_name(icon_name)
 	name_label.show()
-
 
 func _on_icon_mouse_exited() -> void:
 	$Node2D/name.hide()
