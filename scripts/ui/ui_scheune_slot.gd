953d46a47e111c42e2f2ae8fdae7370470c5d317
Bill Meixner <20210229@htlwrn.ac.at>
2024-12-12 08:53:07 +0100

    made crop names in scheune ui changeable

--- scripts/ui/ui_scheune_slot.gd
+++ scripts/ui/ui_scheune_slot.gd
@@ -1,5 +1,9 @@
 extends PanelContainer
 
+var display_names = {
+	"cauliflower": "Cauli",
+	"aubergine": "Eggplant"
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,8 +19,11 @@ func _on_icon_mouse_entered() -> void:
 	var name_label = $Node2D/name
 	var icon_name = $Icon.texture.get_path().get_file().get_basename()
 
-	icon_name = icon_name[0].to_upper() + icon_name.substr(1,-1)
-	name_label.text = icon_name
+	if icon_name in display_names:
+		name_label.text = display_names[icon_name]
+	else:
+		icon_name = icon_name[0].to_upper() + icon_name.substr(1,-1)
+		name_label.text = icon_name
 	name_label.show()
 
 
