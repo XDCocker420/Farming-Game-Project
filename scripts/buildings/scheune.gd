d5585a7a186c86174a2bbf77ed5672a1ea1b569a
Oliver <20210207@htlwrn.ac.at>
2024-12-13 10:16:54 +0100

    working on market

--- scripts/buildings/scheune.gd
+++ scripts/buildings/scheune.gd
@@ -14,84 +14,84 @@ var max_storage = 10
 
 
 func _ready() -> void:
-	#if not 'inventory' in SaveData.data:
-		#SaveData.data['player']['inventory']
-	e_button.visible = false
+    #if not 'inventory' in SaveData.data:
+        #SaveData.data['player']['inventory']
+    e_button.visible = false
 
-	player.interact.connect(_on_player_interact)
-	door_area.body_entered.connect(_on_door_area_body_entered)
-	door_area.body_exited.connect(_on_door_area_body_exited)
+    player.interact.connect(_on_player_interact)
+    door_area.body_entered.connect(_on_door_area_body_entered)
+    door_area.body_exited.connect(_on_door_area_body_exited)
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact") && in_area:
-		#ui_scheune_instance.load_inventory()
-		ui.show()
+    if event.is_action_pressed("interact") && in_area:
+        #ui_scheune_instance.load_inventory()
+        ui.show()
 
 
 func _on_player_interact():
-	if in_area:
-		if not open:
-			door.play("open")
-			open = true
-			#ui_scheune_instance.load_inventory()
-			load_inventory()
-			ui.show()
-		else:
-			door.play_backwards("open")
-			open = false
-			
-			ui.hide()
-			delete_slots()
+    if in_area:
+        if not open:
+            door.play("open")
+            open = true
+            #ui_scheune_instance.load_inventory()
+            load_inventory()
+            ui.show()
+        else:
+            door.play_backwards("open")
+            open = false
+            
+            ui.hide()
+            delete_slots()
 
 
 func _on_door_area_body_entered(body):
-	if body.is_in_group("Player"):
-		e_button.visible = true
-		in_area = true
-		
+    if body.is_in_group("Player"):
+        e_button.visible = true
+        in_area = true
+        
 
 func _on_door_area_body_exited(body):
-	if body.is_in_group("Player"):
-		e_button.visible = false
-		in_area = false
-		
-		if open:
-			door.play_backwards("open")
-			open = false
-			ui.hide()
-			delete_slots()
+    if body.is_in_group("Player"):
+        e_button.visible = false
+        in_area = false
+        
+        if open:
+            door.play_backwards("open")
+            open = false
+            ui.hide()
+            delete_slots()
 
 
 func load_inventory() -> void:
-	var inventory = SaveGame.get_inventory()
-	var count = 0
-	
+    var inventory = SaveGame.get_inventory()
+    var count = 0
+    
 
-	var keys = inventory.keys()
-	keys.sort_custom(func(x:String, y:String) -> bool: return inventory[x] > inventory[y])
-		
-	for item in keys:
-		
-		var slot = preload("res://scenes/ui/ui_slot.tscn")
+    var keys = inventory.keys()
+    keys.sort_custom(func(x:String, y:String) -> bool: return inventory[x] > inventory[y])
+        
+    for item in keys:
+        
+        var slot = preload("res://scenes/ui/ui_slot.tscn")
 
-		var new_slot = slot.instantiate()
+        var new_slot = slot.instantiate()
 
-		if new_slot.has_node("Icon"):
-			new_slot.get_node("Icon").texture = load("res://assets/gui/icons/" + item + ".png")
-			slots.add_child(new_slot)
-			
-			new_slot.show()
-			count = SaveGame.get_item_count(str(item))
+        if new_slot.has_node("Icon"):
+            new_slot.get_node("Icon").texture = load("res://assets/gui/icons/" + item + ".png")
+            slots.add_child(new_slot)
+            
+            new_slot.show()
+            count = SaveGame.get_item_count(str(item))
 
-			if count >= 1:
-				new_slot.get_node("Node2D/amount").text = str(count)
-				new_slot.get_node("Node2D/amount").show()
-			else:
-				new_slot.get_node("Node2D/amount").hide()
-	
-	
+            if count >= 1:
+                new_slot.get_node("Node2D/amount").text = str(count)
+                new_slot.get_node("Node2D/amount").show()
+            else:
+                new_slot.get_node("Node2D/amount").hide()
+    
+    
 func delete_slots() -> void:
-	for slot in slots.get_children():
-		slot.queue_free()
-		#slots.remove_child(slot)
+    for slot in slots.get_children():
+        slot.queue_free()
+        #slots.remove_child(slot)
