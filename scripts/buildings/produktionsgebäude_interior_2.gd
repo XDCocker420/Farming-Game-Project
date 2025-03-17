07b9517ec7955f574fc7f49bade2b83d05ff2eb4
Bill Meixner <Meixner.Bill@Student.htlwrn.ac.at>
2025-03-17 09:29:06 +0100

    Refactor interior building scripts to remove player instantiation and camera follow logic; store building ID for scene transitions.

--- scripts/buildings/produktionsgebäude_interior_2.gd
+++ scripts/buildings/produktionsgebäude_interior_2.gd
@@ -2,45 +2,16 @@ extends StaticBody2D
 
 @onready var exit_area = $ExitArea
 
-var player_ref = null
-
 func _ready():
-	print("ProduktionsgebäudeInterior2: Ready function called")
 	exit_area.body_entered.connect(_on_exit_area_body_entered)
-	
-	# Check if Player exists in the scene
-	var player = get_tree().get_first_node_in_group("Player")
-	if player:
-		player_ref = player
-		print("ProduktionsgebäudeInterior2: Player found in scene")
-		
-		# Update camera to follow player
-		call_deferred("update_camera")
-	else:
-		print("ProduktionsgebäudeInterior2: Player not found, instantiating...")
-		var player_scene = load("res://scenes/characters/player.tscn")
-		if player_scene:
-			var player_instance = player_scene.instantiate()
-			get_tree().current_scene.add_child(player_instance)
-			player_ref = player_instance
-			print("ProduktionsgebäudeInterior2: Player instantiated")
-			
-			# Update camera to follow player
-			call_deferred("update_camera")
-		else:
-			print("ProduktionsgebäudeInterior2: ERROR - Could not load Player scene!")
-
-func update_camera():
-	await get_tree().process_frame
-	if player_ref:
-		var viewport = get_viewport()
-		if viewport:
-			viewport.canvas_transform.origin = -player_ref.global_position + Vector2(viewport.size) / 2
-			print("ProduktionsgebäudeInterior2: Camera updated to follow player")
 
 func _on_exit_area_body_entered(body):
 	if body.is_in_group("Player"):
-		print("ProduktionsgebäudeInterior2: Player entered exit area")
+		# Store building ID to spawn at correct location
+		var global_data = get_node("/root/GlobalData")
+		if global_data:
+			global_data.last_building_entered = 2
+		
 		# Switch back to main scene
 		get_tree().change_scene_to_file("res://scenes/maps/game_map.tscn")
 	else:
