1b6c28defbdc265e86fe94aafffacdec98b23187
Bill Meixner <Meixner.Bill@Student.htlwrn.ac.at>
2025-03-18 09:52:54 +0100

    Refactor Weberei interior script to remove unused exit collision variable and streamline exit area handling

--- scripts/buildings/Weberei_interior.gd
+++ scripts/buildings/Weberei_interior.gd
@@ -1,8 +1,6 @@
 extends StaticBody2D
 
 @onready var exit_area = $ExitArea
-@onready var exit_collision = $ExitArea/CollisionShape2D
-@onready var player = $Player
 
 func _ready():
 	exit_area.body_entered.connect(_on_exit_area_body_entered)
@@ -11,9 +9,6 @@ func _on_exit_area_body_entered(body):
 	if body.is_in_group("Player"):
 		# Store building ID to spawn at correct location
 		SaveGame.last_building_entered = 2
-		
-		# Save the exit position using the CollisionShape2D's position
-		SaveGame.last_exterior_position = exit_collision.global_position
 		
 		# Switch back to main scene
 		get_tree().change_scene_to_file("res://scenes/maps/game_map.tscn")
