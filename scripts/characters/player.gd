9e551b53bcf9e66219cfe204b10ba2b69f1eed57
Clemens Hausegger <hausegger.clemens@student.htlwrn.ac.at>
2025-03-24 16:51:58 +0100

    Added obstacles

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -17,69 +17,69 @@ var is_jump: bool
 
 
 func _ready() -> void:
-    current_speed = normal_speed
-    
-    SaveGame.clear_inventory()
-    
-    SaveGame.add_to_inventory("carrot", 50)
-    SaveGame.add_to_inventory("corn", 30)
-    SaveGame.add_to_inventory("eggplant", 3)
-    SaveGame.add_to_inventory("potatoe", 100)
-    if SaveGame.get_money() != 0:
-        SaveGame.remove_money(SaveGame.get_money())
-    SaveGame.add_money(3100)
+	current_speed = normal_speed
+	
+	SaveGame.clear_inventory()
+	
+	SaveGame.add_to_inventory("carrot", 50)
+	SaveGame.add_to_inventory("corn", 30)
+	SaveGame.add_to_inventory("eggplant", 3)
+	SaveGame.add_to_inventory("potatoe", 100)
+	if SaveGame.get_money() != 0:
+		SaveGame.remove_money(SaveGame.get_money())
+	SaveGame.add_money(3100)
 
 
 func _input(event: InputEvent) -> void:
-    # Check if the interaction key is pressed
-    if event.is_action_pressed("interact"):
-        interact.emit()
-        
-    if event.is_action_pressed("interact2"):
-        interact2.emit()
-        
-    if event.is_action_pressed("jump"):
-        body.play("jump_" + looking_direction)
-        
+	# Check if the interaction key is pressed
+	if event.is_action_pressed("interact"):
+		interact.emit()
+		
+	if event.is_action_pressed("interact2"):
+		interact2.emit()
+		
+	if event.is_action_pressed("jump"):
+		body.play("jump_" + looking_direction)
+		
 
-    # exit game on esc
-    '''if event.is_action_pressed("ui_cancel"):
-        get_tree().quit()'''
+	# exit game on esc
+	'''if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()'''
 
 
 func set_selected_crop(crop_name: String) -> void:
-    selected_crop = crop_name
+	selected_crop = crop_name
 
-    
+	
 func _physics_process(delta: float) -> void:
-    if !cant_move:
-        current_speed = sprint_speed if Input.is_action_pressed("sprint") else normal_speed
-        
-        var direction = Vector2.ZERO
-        direction.x = Input.get_axis("move_left", "move_right")
-        direction.y = Input.get_axis("move_up", "move_down")
-        direction = direction.normalized()
-        
-        velocity = direction * current_speed
+	if !cant_move:
+		current_speed = sprint_speed if Input.is_action_pressed("sprint") else normal_speed
+		
+		var direction = Vector2.ZERO
+		direction.x = Input.get_axis("move_left", "move_right")
+		direction.y = Input.get_axis("move_up", "move_down")
+		direction = direction.normalized()
+		
+		velocity = direction * current_speed
 
-        if direction.x > 0:
-            looking_direction = "right"
-        elif direction.x < 0:
-            looking_direction = "left"
-        elif direction.y > 0:
-            looking_direction = "down"
-        elif direction.y < 0:
-            looking_direction = "up"
+		if direction.x > 0:
+			looking_direction = "right"
+		elif direction.x < 0:
+			looking_direction = "left"
+		elif direction.y > 0:
+			looking_direction = "down"
+		elif direction.y < 0:
+			looking_direction = "up"
 
-        update_animation(direction)
-        move_and_slide()
+		update_animation(direction)
+		move_and_slide()
 
 
 func update_animation(direction: Vector2) -> void:
-    if direction != Vector2.ZERO:
-        if current_speed == normal_speed:
-            body.play(looking_direction)
-        else:
-            body.play(looking_direction, 1.5)
-    else:
-        body.stop()
+	if direction != Vector2.ZERO:
+		if current_speed == normal_speed:
+			body.play(looking_direction)
+		else:
+			body.play(looking_direction, 1.5)
+	else:
+		body.stop()
--- scripts/ui/task_caravan/ui_task_caravan_slot.gd
+++ scripts/ui/task_caravan/ui_task_caravan_slot.gd
@@ -1,1 +1,15 @@
 extends PanelContainer
+
+@onready var btn:Button = $Button
+
+func _ready() -> void:
+	btn.button_down.connect(_on_down)
+	btn.button_up.connect(_on_release)
+	
+	
+func _on_down():
+	print("adbadu")
+	modulate = Color(0.8,0.8,0.8,1.0)
+	
+func _on_release():
+	modulate = Color(1.0,1.0,1.0,1.0)
