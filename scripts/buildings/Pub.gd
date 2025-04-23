a5f7d16966b375a2bebfdf8de97f17c26e35da16
Bill Meixner <Meixner.Bill@Student.htlwrn.ac.at>
2025-04-23 09:20:02 +0200

    Enhance pub building with animated door and updated tile map data

--- scripts/buildings/Pub.gd
+++ scripts/buildings/Pub.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 
 @onready var player: CharacterBody2D = %Player
 @onready var door_area: Area2D = $DoorArea
+@onready var door: AnimatedSprite2D = $door
 
 var in_door_area = false
 var interior_scene_path = "res://scenes/buildings/Pub_interior.tscn"
@@ -21,7 +22,9 @@ func _on_player_interact() -> void:
 func _on_door_area_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") && LevelingHandler.is_building_unlocked("pub"):
 		in_door_area = true
+		door.play("door")
 
 func _on_door_area_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		in_door_area = false
+		door.play_backwards("door")
